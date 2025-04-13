5af0b98d7160330bf9077d8c61ddfe90b9605455
Lasne Olivier <lasneolivier@gmail.com>
2025-04-13 16:08:00 +0200

    added some colors and end the animation

--- src/newkey.rs
+++ src/newkey.rs
@@ -1,5 +1,10 @@
+use std::thread;
 use std::path::PathBuf;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+use colored::Colorize;
 use rsa::pkcs8::LineEnding;
 use rsa::{rand_core::OsRng, RsaPrivateKey, RsaPublicKey};
 use rsa::pkcs1::EncodeRsaPrivateKey;
@@ -14,12 +19,25 @@ pub fn newkey(output: &Option<PathBuf>, size: &Option<u16>) -> Result<(), String
         _ => return Err("Invalid key size. Valid RSA key size are: 1024, 2048, 4096.".to_string())
     };
 
-    loading_animation("Generating the private key");
+    let run_animation = Arc::new(AtomicBool::new(true));
+    let run_animation_clone = Arc::clone(&run_animation);
+
+    // small animation to help the user wait
+    let animation = thread::spawn(move || {
+        let key_size = format!("{} bits", size);
+        loading_animation(
+            format!("Generating a {} private key.", key_size.blue()).as_ref(),
+            run_animation_clone
+        );
+    });
 
     let priv_key = RsaPrivateKey::new(&mut OsRng, usize::from(size))
         .map_err(|_| "Failed to generate Private Key.")?;
 
-    
+    // close the animation
+    run_animation.store(false, Ordering::Relaxed);
+    let _ = animation.join();
+
     let privkey_file = match output {
         Some(path) => path.clone(),
         None => return Err("An output file is required to store the private key".to_string()),
@@ -28,6 +46,9 @@ pub fn newkey(output: &Option<PathBuf>, size: &Option<u16>) -> Result<(), String
     if priv_key.write_pkcs1_pem_file(privkey_file.clone(), LineEnding::default()).is_err() {
         return Err(format!("Failed to write private key to {}", privkey_file.display()));
     }
+
+    let privkey_file = format!("{}", privkey_file.display());
+    eprintln!("Private key written to {}", privkey_file.yellow().bold());
 
     let _pub_key = RsaPublicKey::from(&priv_key);
 
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,18 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::io::{self, Write};
 use std::{thread, time::Duration};
 use colored::Colorize;
 
-pub fn loading_animation(message: &str) {
+pub fn loading_animation(message: &str, running: Arc<AtomicBool>) {
     let ascii_braille = ["⠷", "⠯", "⠟", "⠻", "⠽", "⠾"];
 
-    loop {
+    while running.load(Ordering::Relaxed) {
         for b in ascii_braille {
             eprint!("\r{} {}", b.green(), message);
             let _ = io::stderr().flush();
             thread::sleep(Duration::from_millis(130));
         }
     }
+    eprintln!("\r{} {}", "⠿".green(), message);
 }
