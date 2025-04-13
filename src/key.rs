8be4bd67e25d8aea6d0a1b30d7a07dcc392af5e7
Lasne Olivier <lasneolivier@gmail.com>
2025-04-13 20:31:08 +0200

    print primes

--- src/key.rs
+++ src/key.rs
@@ -1,6 +1,23 @@
 use std::path::PathBuf;
+use std::fs;
+
+use rsa::pkcs1::DecodeRsaPrivateKey;
+use rsa::traits::PrivateKeyParts;
+use rsa::RsaPrivateKey;
 
 /// Inspect the content of a key
 pub fn key(keyfile: &PathBuf) -> Result<(), String> {
-    todo!()
+    
+    let file_content = fs::read_to_string(keyfile)
+        .map_err(|_| format!("Failed to read the content of {}", keyfile.display()))?;
+
+    let private_key = RsaPrivateKey::from_pkcs1_pem(&file_content)
+        .map_err(|_| "Failed to parse PEM content")?;
+
+    println!("primes:");
+    for prime in private_key.primes() {
+        println!("{}", prime);
+    }
+
+    Ok(())
 }
