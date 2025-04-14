a7ef857a1d78fae716534989cbcd1aaa089bf1a9
Lasne Olivier <lasneolivier@gmail.com>
2025-04-14 22:38:14 +0200

    added support for pkcs8 to the `key` subcommand

--- src/key.rs
+++ src/key.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use std::fs;
 
 use rsa::pkcs1::DecodeRsaPrivateKey;
+use rsa::pkcs8::DecodePrivateKey;
 use rsa::traits::PrivateKeyParts;
 use rsa::RsaPrivateKey;
 
@@ -15,8 +16,16 @@ pub fn key(keyfile: &PathBuf) -> Result<(), String> {
     let file_content = fs::read_to_string(keyfile)
         .map_err(|_| format!("Failed to read the content of {}", keyfile.display()))?;
 
-    let private_key = RsaPrivateKey::from_pkcs1_pem(&file_content)
-        .map_err(|_| "Failed to parse PEM content")?;
+    let res = RsaPrivateKey::from_pkcs1_pem(&file_content);
+
+    let private_key = if res.is_err() {
+        match RsaPrivateKey::from_pkcs8_pem(&file_content) {
+            Ok(private_key) => private_key,
+            Err(_) => return Err("Failed to parse PEM content".to_owned()),
+        }
+    } else {
+        res.unwrap()
+    };
 
     println!("primes:");
     for prime in private_key.primes() {
