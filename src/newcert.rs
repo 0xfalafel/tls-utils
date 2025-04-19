21b1dc2a60c2e4a4beae37a5425c21bd82b99a7b
Lasne Olivier <lasneolivier@gmail.com>
2025-04-19 20:10:16 +0200

    move to something with a bit more fined control

--- src/newcert.rs
+++ src/newcert.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use rcgen::{generate_simple_self_signed, CertifiedKey};
+use rcgen::{CertificateParams, KeyPair};
 use crate::key::read_private_key;
 
 /// Generate a new Certificate using an existing RSA private key
@@ -8,8 +8,16 @@ pub fn newcert(keyfile: &PathBuf, domain: &str) -> Result<(), String> {
     //let private_key = read_private_key(keyfile)?;
 
     let subject_alt_names = vec![domain.to_string()];
-    
-    let CertifiedKey { cert, key_pair } = generate_simple_self_signed(subject_alt_names).unwrap();
+
+    let cert_params = CertificateParams::new(subject_alt_names)
+        .map_err(|_| "Failed to initalize the certificate")?;
+
+    //let private_key = read_private_key(keyfile)?;
+    let key_pair = KeyPair::generate()
+        .map_err(|_| "Failed to generate certificate key")?;
+
+    let cert = cert_params.self_signed(&key_pair)
+        .map_err(|_| "Failed to generate certificate")?;
 
     println!("{}", cert.pem());
     println!("{}", key_pair.serialize_pem());
