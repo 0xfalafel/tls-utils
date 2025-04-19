eceef535cdd04c2c3062eea8903d527791832531
Lasne Olivier <lasneolivier@gmail.com>
2025-04-19 20:17:50 +0200

    create certificate from a given key

--- src/newcert.rs
+++ src/newcert.rs
@@ -1,20 +1,23 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 use rcgen::{CertificateParams, KeyPair};
-use crate::key::read_private_key;
 
 /// Generate a new Certificate using an existing RSA private key
 pub fn newcert(keyfile: &PathBuf, domain: &str) -> Result<(), String> {
-    // Read the private key from file
-    //let private_key = read_private_key(keyfile)?;
+
+    let key_data = fs::read_to_string(keyfile)
+        .map_err(|_| format!("Failed to read the content of {}", keyfile.display()))?;
+
+    // Parse the PEM data
+    let key_pair = KeyPair::from_pem(&key_data)
+        .map_err(|_| "Failed to read key from PEM")?;
 
     let subject_alt_names = vec![domain.to_string()];
 
     let cert_params = CertificateParams::new(subject_alt_names)
         .map_err(|_| "Failed to initalize the certificate")?;
 
-    //let private_key = read_private_key(keyfile)?;
-    let key_pair = KeyPair::generate()
-        .map_err(|_| "Failed to generate certificate key")?;
+    // let key_pair = KeyPair::generate()
+    //     .map_err(|_| "Failed to generate certificate key")?;
 
     let cert = cert_params.self_signed(&key_pair)
         .map_err(|_| "Failed to generate certificate")?;
