97c3343eb07d9b30fc3777bd483a8c9ce5354688
Olivier Lasne <lasneolivier@gmail.com>
2025-07-06 21:39:21 +0200

    parse a pem certificate

--- src/cert.rs
+++ src/cert.rs
@@ -1,8 +1,18 @@
 use std::path::PathBuf;
+use std::fs;
+use x509_parser::prelude::parse_x509_pem;
 
 /// Generate a new Certificate using an existing RSA private key
 pub fn cert(cert_file: &PathBuf) -> Result<(), String> {
-    println!("Hi mom!");
+    
+    let file_content = fs::read(cert_file)
+        .map_err(|_| format!("Failed to read the content of {}", cert_file.display()))?;
+
+    if let Ok((_remaining_bytes, pem_certificate)) = parse_x509_pem(&file_content) {
+        if let Ok(certificate) = pem_certificate.parse_x509() {
+            println!("version: {}",certificate.version());
+        }
+    }        
 
     Ok(())
 }
