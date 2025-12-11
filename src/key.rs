f6dc7d733f9080ab5855f2668a335e59da6471fd
Olivier Lasne <lasneolivier@gmail.com>
2025-12-12 00:27:29 +0100

    variable lentgh 0 padding depending of the component

--- src/key.rs
+++ src/key.rs
@@ -53,7 +53,7 @@ pub fn key(keyfile: &PathBuf, pubout: &Option<PathBuf>, der: bool) -> Result<(),
     // TODO: handle error, precompute can fail
     private_key.precompute().expect("Failed to precompute private key values");
 
-    let key_size = match private_key.n().bits() {
+    let key_size: usize = match private_key.n().bits() {
         n if n <= 512 => 512,
         n if n <= 1024 => 1024,
         n if n <= 2048 => 2048,
@@ -70,12 +70,12 @@ pub fn key(keyfile: &PathBuf, pubout: &Option<PathBuf>, der: bool) -> Result<(),
         return export_pubkey(pubkey_path, private_key, der)
     }
     
-    print_modulus(&private_key);
+    print_modulus(&private_key, key_size);
     print_public_exponent(&private_key);
-    print_private_exponent(&private_key);
-    print_primes(&private_key);
+    print_private_exponent(&private_key, key_size);
+    print_primes(&private_key, key_size / 2);
     print_exponents(&private_key);
-    print_coefficient(&private_key);
+    print_coefficient(&private_key, key_size / 2);
 
     Ok(())
 }
@@ -97,9 +97,9 @@ fn export_pubkey(pubkey_path: &PathBuf, private_key: RsaPrivateKey, der: bool) -
     Ok(())
 }
 
-fn print_modulus(private_key: &RsaPrivateKey) {
+fn print_modulus(private_key: &RsaPrivateKey, bit_size: usize) {
     let modulus = private_key.n();
-    let hex_modulus = format_hex(modulus);
+    let hex_modulus = format_hex(modulus, bit_size / 8);
     println!("{}\n{}\n", "modulus (n):".blue().bold(), hex_modulus);
 }
 
@@ -108,42 +108,44 @@ fn print_public_exponent(private_key: &RsaPrivateKey) {
     println!("{} {} (0x{:x})\n", "public exponent (e):".blue().bold(), exponent, exponent);
 }
 
-fn print_private_exponent(private_key: &RsaPrivateKey) {
+fn print_private_exponent(private_key: &RsaPrivateKey, key_size: usize) {
     let modulus = private_key.d();
-    let hex_modulus = format_hex(modulus);
+    let hex_modulus = format_hex(modulus, key_size / 8);
     println!("{}\n{}\n", "private exponent (d):".blue().bold(), hex_modulus);
 }
 
-fn print_primes(private_key: &RsaPrivateKey) {
+fn print_primes(private_key: &RsaPrivateKey, key_bit_size: usize) {
     let primes = private_key.primes();
     let p = primes.iter().nth(0).unwrap();
     let q = primes.iter().nth(1).unwrap();
 
-    let p_hex = format_hex(p);
-    let q_hex = format_hex(q);
+    let p_hex = format_hex(p, key_bit_size / 8);
+    let q_hex = format_hex(q, key_bit_size / 8);
     println!("{}\n{}\n", "prime1 (p):".blue().bold(), p_hex);
     println!("{}\n{}\n", "prime2 (q):".blue().bold(), q_hex);
 }
 
 fn print_exponents(private_key: &RsaPrivateKey) {
     if let Some(dp) = private_key.dp() {
-        println!("{}\n{}\n", "exponent1 (dp):".blue().bold(), format_hex(dp));
+        println!("{}\n{}\n", "exponent1 (dp):".blue().bold(), format_hex(dp, 4));
     }
 
     if let Some(dq) = private_key.dq() {
-        println!("{}\n{}\n", "exponent2 (dq):".blue().bold(), format_hex(dq));
+        println!("{}\n{}\n", "exponent2 (dq):".blue().bold(), format_hex(dq, 4));
     }
 }
 
-fn print_coefficient(private_key: &RsaPrivateKey) {
+fn print_coefficient(private_key: &RsaPrivateKey, coeff_size: usize) {
     if let Some(crt_coefficient) = private_key.crt_coefficient() {
-        println!("{}\n{}", "coefficient:".blue().bold(), format_hex(&crt_coefficient));
+        println!("{}\n{}", "coefficient:".blue().bold(), format_hex(&crt_coefficient, coeff_size / 8));
     }
 }
 
-fn format_hex(number: &BigUint) -> String {
+fn format_hex(number: &BigUint, byte_size: usize) -> String {
+    let n_chars: usize = byte_size * 2;
+
     // print the number as hex in a string
-    let hex_number= format!("{:0258x}", number);
+    let hex_number= format!("{:0>n_chars$x}", number);
 
     // group by hex
     let bytes: Vec<&str> = hex_number.as_bytes()
