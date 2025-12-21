fc87586bc72b5eafad987c787cc0061d63a7fd4c
Olivier Lasne <lasneolivier@gmail.com>
2025-12-21 01:50:40 +0100

    removed cert folder

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod util;
 use newkey::newkey;
 use key::key;
 use newcert::newcert;
-use cert::cert::read_certificate;
+use cert::read_certificate;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
