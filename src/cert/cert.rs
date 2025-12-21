08b340123a107a30e8364753e932e5ef1a7f31b7
Olivier Lasne <lasneolivier@gmail.com>
2025-12-21 01:48:05 +0100

    fix signature algorithm as text instead of OID

--- src/cert/cert.rs
+++ src/cert/cert.rs
@@ -8,7 +8,7 @@ use asn1_rs::{oid, Oid};
 // Take the OID list from here:
 // https://learn.microsoft.com/fr-fr/windows/win32/api/wincrypt/ns-wincrypt-crypt_algorithm_identifier
 
-/// Read a certificate data
+// Read a certificate data
 pub fn read_certificate(cert_file: &PathBuf) -> Result<(), String> {
     
     let file_content = fs::read(cert_file)
@@ -18,11 +18,32 @@ pub fn read_certificate(cert_file: &PathBuf) -> Result<(), String> {
         if let Ok(certificate) = pem_certificate.parse_x509() {
             println!("{} {}", "Version:".blue().bold(), certificate.version());           
             println!("{}\n\t{}", "Serial number:".blue().bold(), format_hex(&certificate.serial));
-            println!("{} {:?}", "Signature Algorithm:".blue().bold(), certificate.signature_algorithm);
 
-            if certificate.signature_algorithm.algorithm == oid! {1.2.840.113549.1.1.11} {
-                println!("{} {}", "Signature Algorithm:".blue().bold(), "sha256WithRSAEncryption");
-            }
+            let algorithm = match certificate.signature_algorithm.algorithm.to_id_string().as_ref() {
+                "1.2.840.113549.1.1.4"   => "md5WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.5"   => "sha1WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.11"  => "sha256WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.12"  => "sha384WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.13"  => "sha512WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.14"  => "sha224WithRSAEncryption".to_string(),
+                "1.2.840.113549.1.1.10"  => "rsassaPss".to_string(),
+
+                "1.2.840.10045.4.1"      => "ecdsa-with-SHA1".to_string(),
+                "1.2.840.10045.4.3.1"    => "ecdsa-with-SHA224".to_string(),
+                "1.2.840.10045.4.3.2"    => "ecdsa-with-SHA256".to_string(),
+                "1.2.840.10045.4.3.3"    => "ecdsa-with-SHA384".to_string(),
+                "1.2.840.10045.4.3.4"    => "ecdsa-with-SHA512".to_string(),
+
+                "1.3.101.112"            => "Ed25519".to_string(),
+                "1.3.101.113"            => "Ed448".to_string(),
+
+                "1.2.840.10040.4.3"      => "dsa-with-sha1".to_string(),
+                "2.16.840.1.101.3.4.3.2" => "dsa-with-sha256".to_string(),
+
+                other => other.to_string(),
+            };
+            
+            println!("{} {}", "Signature Algorithm:".blue().bold(), algorithm);
         }
     }        
 
